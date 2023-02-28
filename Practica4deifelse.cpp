e0372f381f156cbd73526f8a935c37d189cb7968
EddieZC20 <126592675+EddieZC20@users.noreply.github.com>
2023-02-28 11:08:20 -0600

    Add files via upload

---
+++ Practica4deifelse.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+ int a, b=18 ;
+
+cout<<"Dinos tu edad"<<endl;
+cin>>a;
+if (a>=b){
+    cout<<"Eres mayor de 18, puedes entrar"<<endl;
+}
+else {
+    cout<<"No puedes entrar";
+
+}
+return 0;
+
+
+}
