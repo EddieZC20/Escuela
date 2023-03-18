a7c86befc770f4f0a6ba85418144a2cd3e7052e9
EddieZC20 <126592675+EddieZC20@users.noreply.github.com>
2023-03-18 00:56:07 -0600

    Add files via upload

---
+++ proramaminutosasegundos.cpp
@@ -0,0 +1,20 @@
+//El  usuario  va  introducir  una  cantidad  de  segundos,  conviértelo  a  minutos  y segundos ejemplo:146segundos => 2 minutos, 26 segundos
+#include <stdio.h>
+#include <iostream>
+
+
+using namespace std;
+int main()
+{
+    int s, m, x;
+do{
+    cout<<"Introduce los segundo que quieras transformar a minutos(0-3600s) "<<endl;
+    cin>>x;
+    system("cls");
+    }while(x<0 || x>3600);
+    m=x/60;
+    s=x%60;
+
+    cout<<m<<" minutos con "<<s<<" segundos";
+
+}
